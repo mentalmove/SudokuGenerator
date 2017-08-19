5ac9b5d8a3ac28a3449f0245088d4edb65474057
Malte Pagel <malte.pagel@mediagroupone.com>
2017-08-19 17:13:44 +0200

    initial commit

--- main.c
+++ main.c
@@ -7,53 +7,60 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <string.h>
 #include <math.h>
 
 
-#define SMALL_LINE 3
-#define LINE 9
-#define TOTAL 81
-
-#define LIMIT 16777216
-
-#define SHOW_SOLVED 1
+typedef unsigned int uint;
 
 
-struct dimensions_collection {
-	int row;
-	int column;
-	int small_square;
-};
+void border_line(void);
+void solid_line(void);
+void dotted_line(void);
+void show_solution (uint*);
+void set_quasi_constants (uint, uint*, uint*, uint*);
+void fill_rows(uint*);
+void fill_cols(uint*);
+void fill_small_squares(uint*);
 
 
-int indices[TOTAL];
-int riddle[TOTAL];
-int solved[TOTAL];
-int unsolved[TOTAL];
-int tries_to_set = 0;
-int taking_back;
-int global_unset_count = 0;
+uint SMALL_LINE, LINE, TOTAL;
 
 
-struct dimensions_collection get_collection(int);
-int contains_element(int*, int, int);
-void get_horizontal(int, int*);
-void get_vertical(int, int*);
-void get_square(int, int*);
-int set_values(int, int);
-void take_back(int);
-
-int show_solution(int*);
-
-
-int show_solution (int* solution) {
+void border_line () {
+	printf( " " );
+	for ( uint i = 0; i < 4 * pow(SMALL_LINE, 2) - 1; i++ )
+		printf( "-" );
+}
+void solid_line () {
+	printf( "|" );
+	for ( uint i = 0; i < SMALL_LINE; i++ ) {
+		printf( "---" );
+		for ( uint j = 0; j < SMALL_LINE - 1; j++ ) {
+			printf( "----" );
+		}
+		if ( i < SMALL_LINE - 1 )
+			printf( "+" );
+	}
+	printf( "|\n" );
+}
+void dotted_line () {
+	printf( "|" );
+	for ( uint i = 0; i < SMALL_LINE; i++ ) {
+		printf( "- -" );
+		for ( uint j = 0; j < SMALL_LINE - 1; j++ ) {
+			printf( " - -" );
+		}
+		printf( "|" );
+	}
+	printf( "\n" );
+}
+void show_solution (uint* solution) {
 	
-	int i;
-	int counter = 0;
+	uint i;
+	uint counter = 0;
 	
-	printf( " -----------------------------------\n" );
+	border_line();
+	printf( "\n" );
 	
 	for ( i = 0; i < TOTAL; i++ ) {
 		if ( i % LINE == 0 )
@@ -70,9 +77,9 @@ int show_solution (int* solution) {
 			printf( "|\n" );
 			if ( i != (TOTAL - 1) ) {
 				if ( i % (SMALL_LINE * LINE) == (SMALL_LINE * LINE - 1) )
-					printf( "|-----------+-----------+-----------|\n" );
+					solid_line();
 				else
-					printf( "|- - - - - -|- - - - - -|- - - - - -|\n" );
+					dotted_line();
 			}
 		}
 		else {
@@ -83,288 +90,68 @@ int show_solution (int* solution) {
 		}
 	}
 	
-	printf( " -----------------------------------" );
-	
-	return counter;
+	border_line();
 }
 
-
-/**
- * Takes a position inside the large square and returns
- *		- the row number
- *		- the column number
- *		- the small square number
- * where this position is situated in
- */
-struct dimensions_collection get_collection (int index) {
-	struct dimensions_collection ret;
-	
-	ret.row = (int) (index / LINE);
-	ret.column = index % LINE;
-	ret.small_square = SMALL_LINE * (int) (ret.row / SMALL_LINE) + (int) (ret.column / SMALL_LINE);
-	
-	return ret;
+void set_quasi_constants (uint small_line_value, uint * small_line, uint * line, uint * total) {
+	*small_line = small_line_value;
+	*line = pow(small_line_value, 2);
+	*total = pow(small_line_value, 4);
 }
 
-/**
- * Is 'the_element' in 'the_array'?
- */
-int contains_element (int* the_array, int the_element, int length) {
-	for ( int i = 0; i < length; i++ )
-		if ( the_array[i] == the_element )
-			return 1;
-	return 0;
+void fill_rows (uint * linear) {
+	for ( uint i = 0; i < TOTAL; i++ )
+		linear[i] = i % LINE + 1;
 }
-
-/**
- * Sets all members of row 'row'
- */
-void get_horizontal (int row, int* ret) {
-	int j = 0;
-	for ( int i = (row * LINE); i < (row * LINE) + LINE; i++ )
-		ret[j++] = riddle[i];
+void fill_cols (uint * linear) {
+	for ( uint i = 0; i < TOTAL; i++ )
+		linear[i] = i / LINE + 1;
 }
-/**
- * Sets all members of column 'col'
- */
-void get_vertical (int col, int* ret) {
-	int j = 0;
-	for ( int i = col; i < TOTAL; i += LINE )
-		ret[j++] = riddle[i];
-}
-/**
- * Sets all members of small square 'which'
- */
-void get_square (int which, int* ret) {
-	for ( int i = 0; i < SMALL_LINE; i++ )
-		for ( int j = 0; j < SMALL_LINE; j++ )
-			ret[SMALL_LINE * i + j] = riddle[LINE * i + which * SMALL_LINE + j + ((int) (which / SMALL_LINE) * (SMALL_LINE - 1) * LINE)];
-}
-
-/**
- * Recursive function:
- * Try for each position the numbers from 1 to LINE
- *      (except 'forbidden_number' if given).
- * If all numbers collide with already set numbers, move is bad.
- * If a number doesn't collide with already set numbers,
- *	- move is bad if next move collides with the already set numbers
- *              (including actual one)
- *	- move is good if it's the last one
- */
-int set_values (int index, int forbidden_number) {
-	
-	if ( taking_back && tries_to_set > (2 * LIMIT) )
-		return 1;
-		
-	int real_index = indices[index];
-	struct dimensions_collection blocks = get_collection(real_index);
-	int elements[LINE];
-	
-	for ( int i = 1; i <= LINE; i++ ) {
-		if ( forbidden_number && i == forbidden_number )
-			continue;
-		
-		tries_to_set++;
-		
-		get_horizontal(blocks.row, elements);
-		if ( contains_element(elements, i, LINE) )
-			continue;
-		
-		get_vertical(blocks.column, elements);
-		if ( contains_element(elements, i, LINE) )
-			continue;
-		
-		get_square(blocks.small_square, elements);
-		if ( contains_element(elements, i, LINE) )
-			continue;
-		
-		riddle[real_index] = i;
-		
-		if ( index == (TOTAL - 1) || set_values((index + 1), 0) )
-			return 1;
-	}
-	
-	riddle[real_index] = 0;
-	
-	return 0;
-}
-
-/**
- * Some steps to hide unnecessary numbers:
- *	a) Define last piece as 'special piece'
- *	b) Remember this piece's value
- *	c) Try to create riddle from this position on,
- *          but forbid the value of the special piece
- *	d)	I)  If operation fails, define the piece before the special piece
- *          as 'special piece' and continue with b)
- *		II) If operation is possible, reset 'special piece'
- *			and put it to start of list
- *	e) Stop if all pieces are tried or calculation limit is reached
- */
-void take_back (int unset_count) {
-	
-	global_unset_count++;
-	
-	int i;
-	
-	int tmp = riddle[indices[TOTAL - unset_count]];
-	int redundant = set_values((TOTAL - unset_count), tmp);
-	
-	if ( !redundant ) {
-		unsolved[indices[TOTAL - unset_count]] = 0;
-		take_back(++unset_count);
-	}
-	else {
-		riddle[indices[TOTAL - unset_count]] = tmp;
-		for ( i = 1; i < unset_count; i++ )
-			riddle[indices[TOTAL - unset_count + i]] = 0;
-		
-		for ( i = (TOTAL - unset_count); i > 0; i-- )
-			indices[i] = indices[i - 1];
-		indices[0] = tmp;
-		
-		if ( global_unset_count < TOTAL && tries_to_set < LIMIT )
-			take_back(unset_count);
-	}
+void fill_small_squares (uint * linear) {
+	for ( uint i = 0; i < TOTAL; i++ )
+		linear[i] = (i / LINE) % SMALL_LINE * SMALL_LINE + i % SMALL_LINE + 1;
 }
 
 
 int main (int argc, const char * argv[]) {
 	
-	int i, j, random, small_rows, small_cols, tmp, redundant;
-	int multi_raw[LINE][LINE];
+	uint * riddle;
 	
 	
-	time_t t;
-	time(&t);
-	srand((unsigned int) t);
+	set_quasi_constants(2, &SMALL_LINE, &LINE, &TOTAL);
+	riddle = malloc(TOTAL * sizeof(uint));
+	fill_rows(riddle);
+	show_solution(riddle);
+	printf( " \t correct rows" );
+	printf( "\n\n" );
+	fill_cols(riddle);
+	show_solution(riddle);
+	printf( " \t correct columns" );
+	printf( "\n\n" );
+	fill_small_squares(riddle);
+	show_solution(riddle);
+	printf( " \t correct small squares" );
+	
+	printf( "\n\n\n" );
+	
+	set_quasi_constants(3, &SMALL_LINE, &LINE, &TOTAL);
+	riddle = realloc(riddle, TOTAL * sizeof(uint));
+	fill_rows(riddle);
+	show_solution(riddle);
+	printf( " \t correct rows" );
+	printf( "\n\n" );
+	fill_cols(riddle);
+	show_solution(riddle);
+	printf( " \t correct columns" );
+	printf( "\n\n" );
+	fill_small_squares(riddle);
+	show_solution(riddle);
+	printf( " \t correct small squares" );
+	
+	printf( "\n\n" );
 	
 	
-	
-	/**
-	 * Initialization:
-	 * Fields are set to 0 ( i.e. we dont' know the number yet)
-	 */
-	for ( i = 0; i < TOTAL; i++ )
-		riddle[i] = 0;
-	
-	/**
-	 * Second initialization:
-	 * LINE times numbers from 0 to (LINE - 1),
-	 * i.e. every square
-	 */
-	int big_rows_array[] = {0, 1, 2};
-	int big_cols_array[] = {0, 1, 2};
-	random = rand() % 4;
-	switch (random) {
-		case 1:
-			big_rows_array[0] = 2;
-			big_rows_array[1] = 1;
-			big_rows_array[2] = 0;
-		break;
-		case 2:
-			big_cols_array[0] = 2;
-			big_cols_array[1] = 1;
-			big_cols_array[2] = 0;
-		break;
-		case 3:
-			big_rows_array[0] = 2;
-			big_rows_array[1] = 1;
-			big_rows_array[2] = 0;
-			big_cols_array[0] = 2;
-			big_cols_array[1] = 1;
-			big_cols_array[2] = 0;
-	}
-	int big_rows, big_cols, big_rows_index, big_cols_index, start_value;
-	i = 0;
-	j = 0;
-	for ( big_rows_index = 0; big_rows_index < SMALL_LINE; big_rows_index++ ) {
-		big_rows = big_rows_array[big_rows_index];
-		for ( big_cols_index = 0; big_cols_index < SMALL_LINE; big_cols_index++ ) {
-			big_cols = big_cols_array[big_cols_index];
-			start_value = big_rows * LINE * SMALL_LINE + (big_cols * SMALL_LINE);
-			for ( small_rows = 0; small_rows < SMALL_LINE; small_rows++ )
-				for ( small_cols = 0; small_cols < SMALL_LINE; small_cols++ )
-					multi_raw[i][j++] = small_rows * LINE + small_cols + start_value;
-			i++;
-			j = 0;
-		}
-	}
-	
-	
-	/**
-	 * Randomization for every element of multi_raw.
-	 * Suffle only inside squares
-	 */
-	for ( i = 0; i < LINE; i++ ) {
-		for ( j = 0; j < LINE; j++ ) {
-			random = rand() % LINE;
-			if ( j == random )
-				continue;
-			tmp = multi_raw[i][j];
-			multi_raw[i][j] = multi_raw[i][random];
-			multi_raw[i][random] = tmp;
-		}
-	}
-	
-	/**
-	 * Linearization
-	 */
-	for ( i = 0; i < LINE; i++ )
-		for ( j = 0; j < LINE; j++ )
-			indices[i * LINE + j] = multi_raw[i][j];
-	
-	
-	/**
-	 * Setting numbers, start with the first one.
-	 * Variable 'redundant' is needed only for formal reasons
-	 */
-	taking_back = 0;
-	redundant = set_values(0, 0);
-	
-	
-	memcpy(solved, riddle, (TOTAL * sizeof(int)));
-	memcpy(unsolved, riddle, (TOTAL * sizeof(int)));
-	
-	
-	/**
-	 * Exchanging some (few) indices for more randomized game
-	 */
-	int random2;
-	for ( i = (LINE - 1); i > 0; i-- ) {
-		for ( j = 0; j < (int) (sqrt(i)); j++ ) {
-			
-			if ( !(rand() % ((int) (i * sqrt(i)))) || !(LINE - j) )
-				continue;
-			
-			random = i * LINE + (int) (rand() % (LINE - j));
-			random2 = rand() % TOTAL;
-			
-			if ( random == random2 )
-				continue;
-			
-			tmp = indices[random];
-			indices[random] = indices[random2];
-			indices[random2] = tmp;
-		}
-	}
-	
-	
-	tries_to_set = 0;
-	taking_back = 1;
-	take_back(1);
-	
-	
-	if ( SHOW_SOLVED ) {
-		printf( "\n\n" );
-		redundant = show_solution(solved);
-		printf( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" );
-	}
-	else
-		printf( "\n\n\n\n" );
-	
-	int counter = show_solution(unsolved);
-	printf( "\t *** %d numbers left *** \n", counter );
+	free(riddle);
 	
 	
 	return 0;
