1e45a6e144e61849fc8baf0ba9e119dd459e3ff1
Malte Pagel <malte.pagel@mediagroupone.com>
2017-08-19 20:53:18 +0200

    initial commit

--- main.c
+++ main.c
@@ -18,9 +18,28 @@ void solid_line(void);
 void dotted_line(void);
 void show_solution (uint*);
 void set_quasi_constants (uint, uint*, uint*, uint*);
-void fill_rows(uint*);
-void fill_cols(uint*);
-void fill_small_squares(uint*);
+void get_horizontal(uint, uint*);
+void get_vertical(uint, uint*);
+void get_square(uint, uint*);
+
+
+uint sudoku4x4[] = {
+	1, 2, 4, 3,
+	4, 3, 1, 2,
+	2, 4, 3, 1,
+	3, 1, 2, 4
+};
+uint sudoku9x9[] = {
+	1, 6, 7, 2, 9, 5, 8, 4, 3,
+	5, 8, 4, 3, 7, 6, 1, 2, 9,
+	2, 9, 3, 1, 4, 8, 6, 7, 5,
+	4, 3, 1, 6, 8, 2, 9, 5, 7,
+	7, 2, 6, 9, 5, 3, 4, 8, 1,
+	9, 5, 8, 7, 1, 4, 3, 6, 2,
+	8, 1, 9, 4, 2, 7, 5, 3, 6,
+	3, 4, 2, 5, 6, 1, 7, 9, 8,
+	6, 7, 5, 8, 3, 9, 2, 1, 4
+};
 
 
 uint SMALL_LINE, LINE, TOTAL;
@@ -93,65 +112,67 @@ void show_solution (uint* solution) {
 	border_line();
 }
 
-void set_quasi_constants (uint small_line_value, uint * small_line, uint * line, uint * total) {
+void set_quasi_constants (uint small_line_value, uint* small_line, uint* line, uint* total) {
 	*small_line = small_line_value;
 	*line = pow(small_line_value, 2);
 	*total = pow(small_line_value, 4);
 }
 
-void fill_rows (uint * linear) {
-	for ( uint i = 0; i < TOTAL; i++ )
-		linear[i] = i % LINE + 1;
+void get_horizontal (uint row, uint* sudoku) {
+	printf( "Row %d contains ", row );
+	for ( int i = (row * LINE); i < (row * LINE) + LINE; i++ ) {
+		if ( i % LINE )
+			printf( ", " );
+		printf( "%d", sudoku[i] );
+	}
+	printf( "\n" );
 }
-void fill_cols (uint * linear) {
-	for ( uint i = 0; i < TOTAL; i++ )
-		linear[i] = i / LINE + 1;
+void get_vertical (uint col, uint* sudoku) {
+	printf( "Column %d contains ", col );
+	for ( int i = col; i < TOTAL; i += LINE ) {
+		if ( i >= LINE )
+			printf( ", " );
+		printf( "%d", sudoku[i] );
+	}
+	printf( "\n" );
 }
-void fill_small_squares (uint * linear) {
-	for ( uint i = 0; i < TOTAL; i++ )
-		linear[i] = (i / LINE) % SMALL_LINE * SMALL_LINE + i % SMALL_LINE + 1;
+void get_square (uint which, uint* sudoku) {
+	printf( "Sqaure %d contains ", which );
+	for ( int i = 0; i < SMALL_LINE; i++ )
+		for ( int j = 0; j < SMALL_LINE; j++ ) {
+			if ( i || j )
+				printf( ", " );
+			printf( "%d", sudoku[LINE * i + which * SMALL_LINE + j + ((int) (which / SMALL_LINE) * (SMALL_LINE - 1) * LINE)] );
+		}
+	printf( "\n" );
 }
 
 
 int main (int argc, const char * argv[]) {
 	
-	uint * sudoku;
-	
+	uint i;
 	
 	set_quasi_constants(2, &SMALL_LINE, &LINE, &TOTAL);
-	sudoku = malloc(TOTAL * sizeof(uint));
-	fill_rows(sudoku);
-	show_solution(sudoku);
-	printf( " \t correct rows" );
-	printf( "\n\n" );
-	fill_cols(sudoku);
-	show_solution(sudoku);
-	printf( " \t correct columns" );
-	printf( "\n\n" );
-	fill_small_squares(sudoku);
-	show_solution(sudoku);
-	printf( " \t correct small squares" );
+	for ( i = 0; i < LINE; i++ )
+		get_horizontal(i, sudoku4x4);
+	for ( i = 0; i < LINE; i++ )
+		get_vertical(i, sudoku4x4);
+	for ( i = 0; i < LINE; i++ )
+		get_square(i, sudoku4x4);
+	show_solution(sudoku4x4);
 	
-	printf( "\n\n\n" );
-	
+	printf( "\n\n" );
+
 	set_quasi_constants(3, &SMALL_LINE, &LINE, &TOTAL);
-	sudoku = realloc(sudoku, TOTAL * sizeof(uint));
-	fill_rows(sudoku);
-	show_solution(sudoku);
-	printf( " \t correct rows" );
-	printf( "\n\n" );
-	fill_cols(sudoku);
-	show_solution(sudoku);
-	printf( " \t correct columns" );
-	printf( "\n\n" );
-	fill_small_squares(sudoku);
-	show_solution(sudoku);
-	printf( " \t correct small squares" );
+	for ( i = 0; i < LINE; i++ )
+		get_horizontal(i, sudoku9x9);
+	for ( i = 0; i < LINE; i++ )
+		get_vertical(i, sudoku9x9);
+	for ( i = 0; i < LINE; i++ )
+		get_square(i, sudoku9x9);
+	show_solution(sudoku9x9);
 	
 	printf( "\n\n" );
-	
-	
-	free(sudoku);
 	
 	
 	return 0;
